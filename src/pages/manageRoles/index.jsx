14b5c11566f316e97b6ce73590883d7db1e0f4c9
Gokul <gokulraj.c@colanonline.com>
2023-12-27 18:36:04 +0530

    newchanges

--- src/pages/manageRoles/index.jsx
+++ src/pages/manageRoles/index.jsx
@@ -1,9 +1,43 @@
-const ManageRoles = ()=>{
-    return (
-        <>
-        <h1>ManageRoles PAge</h1>
-        </>
-    )
-}
+import { useState } from "react";
+import AddButton from "../../../components/common/Button/addButton";
+import CustomizedTables from "../../../components/common/table";
+import { RolesAndPermissionsData, RolesAndPermissionsHead } from "../../../components/common/tableData";
 
-export default ManageRoles
+const ManageRoles = () => {
+  const [size, setSize] = useState(0);
+  const [page, setPage] = useState(5);
+
+  const paginationRowsOptions = [5, 10, 20, 50, 100];
+
+  const handlePerRowsChange = async (event) => {
+    setPage(+event.target.value);
+    setSize(0);
+  };
+
+  const handlePageChange = async (event, newPage) => {
+    setPage(newPage);
+  };
+  
+  return (
+    <>
+      <h1>Roles & permissions</h1>
+      <div>
+        <AddButton />
+      </div>
+      <div>
+        <CustomizedTables
+          columns={RolesAndPermissionsHead}
+          rows={RolesAndPermissionsData}
+          paginationStatus={true}
+          rowsPerPageOptions={paginationRowsOptions}
+          page={page}
+          size={size}
+          handleChangePage={handlePageChange}
+          handleChangeRowsPerPage={handlePerRowsChange}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ManageRoles;
--- src/pages/manageSubAdmin/index.jsx
+++ src/pages/manageSubAdmin/index.jsx
@@ -1,9 +1,42 @@
-const ManageSubAdmin = ()=>{
-    return (
-        <>
-        <h1>ManageSubAdmin PAge</h1>
-        </>
-    )
-}
+import { useState } from "react";
+import CustomizedTables from "../../../components/common/table";
 
-export default ManageSubAdmin
+import AddButton from "../../../components/common/Button/addButton";
+import { subAdminTableData, subAdminTableHead } from "../../../components/common/tableData";
+
+const ManageSubAdmin = () => {
+  const [size, setSize] = useState(0);
+  const [page, setPage] = useState(5);
+
+  const paginationRowsOptions = [5, 10, 20, 50, 100];
+
+  const handlePerRowsChange = async (event) => {
+    setPage(+event.target.value);
+    setSize(0);
+  };
+
+  const handlePageChange = async (event, newPage) => {
+    setPage(newPage);
+  };
+
+  return (
+    <>
+      <h2>Manage SubAdmin</h2>
+
+      <AddButton />
+
+      <CustomizedTables
+        columns={subAdminTableHead}
+        rows={subAdminTableData}
+        paginationStatus={true}
+        rowsPerPageOptions={paginationRowsOptions}
+        page={page}
+        size={size}
+        handleChangePage={handlePageChange}
+        handleChangeRowsPerPage={handlePerRowsChange}
+      />
+    </>
+  );
+};
+
+export default ManageSubAdmin;
