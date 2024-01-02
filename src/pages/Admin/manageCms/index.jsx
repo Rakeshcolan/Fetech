78dcc880439528d03f396fc02f4a1c176bd4cd29
Raji-123456 <rajalakshmi@colanonline.com>
2024-01-02 19:54:27 +0530

    added cms

--- src/pages/Admin/manageCms/index.jsx
+++ src/pages/Admin/manageCms/index.jsx
@@ -1,7 +1,21 @@
+import CommonUpload from "../../../components/common/Field/CommonUpload"
+import "./manageCmsStyle.css"
 const ManageCMS = ()=>{
     return (
         <>
         <h1>ManageCMS PAge</h1>
+        <div className="cmscontainer">
+
+        <div className="cmsupload">
+            <p>About Us</p>
+            <CommonUpload label={"Upload Your Files"}/>
+        </div>
+        <div className="cmsupload">
+            <p>Term And Condition</p>
+            <CommonUpload label={"Upload Your Files"}/>
+        </div>
+        <button className="cmsbutton">Save</button>
+        </div>
         </>
     )
 }
